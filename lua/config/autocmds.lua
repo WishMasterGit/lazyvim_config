e2e8333879727a0a0052426f0a16f0ba4b4a47bf
wishmaster <wishmasterts@gmail.com>
2025-08-02 01:13:27 -0400

    add keymap to enable and disable copilot

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,22 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+--[[
+This code defines a toggle function for GitHub Copilot in Neovim.
+It uses a boolean variable `copilot_enabled` to track the current state.
+When `ToggleCopilot()` is called, it enables or disables Copilot accordingly,
+prints a message to inform the user, and updates the state variable.
+]]
+local copilot_enabled = false
+vim.cmd("Copilot disable")
+function ToggleCopilot()
+  if copilot_enabled then
+    vim.cmd("Copilot disable")
+    print("Copilot disabled")
+  else
+    vim.cmd("Copilot enable")
+    print("Copilot enabled")
+  end
+  copilot_enabled = not copilot_enabled
+end
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- vim.keymap.set("i", "<C-\\>", ":Copilot suggest<CR>", { silent = true })
+
+vim.keymap.set(
+  "n",
+  "<leader>at",
+  ":lua ToggleCopilot()<CR>",
+  { noremap = true, silent = true, desc = "Toggle Copilot" }
+)
+-- vim.keymap.set("n", "<leader>at", function()
+--   if vim.g.copilot_enabled == 1 then
+--     vim.cmd("Copilot disable")
+--   else
+--     vim.cmd("Copilot enable")
+--   end
+-- end, { desc = "Toggle Copilot" })
