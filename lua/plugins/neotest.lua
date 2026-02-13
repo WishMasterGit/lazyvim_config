2a82c9c322527882dca450ca0076046025540a00
wishmaster <wishmasterts@gmail.com>
2026-02-13 11:38:51 -0500

    update config

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,11 +1,31 @@
 return {
   "nvim-neotest/neotest",
+  -- opts = {
+  --   adapters = {
+  --     ["neotest-rust"] = {
+  --       args = { "--no-capture" },
+  --       env = { RUST_BACKTRACE = "1" },
+  --     },
+  --   },
+  --   status = { virtual_text = true },
+  -- },
   opts = {
+    -- Can be a list of adapters like what neotest expects,
+    -- or a list of adapter names,
+    -- or a table of adapter names, mapped to adapter configs.
+    -- The adapter will then be automatically loaded with the config.
     adapters = {
-      ["neotest-rust"] = {
+      ["rustaceanvim.neotest"] = {
         args = { "--no-capture" },
         env = { RUST_BACKTRACE = "1" },
       },
     },
+    -- Example for loading neotest-golang with a custom config
+    -- adapters = {
+    --   ["neotest-golang"] = {
+    --     go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
+    --     dap_go_enabled = true,
+    --   },
+    -- },
   },
 }
